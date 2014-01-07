60b7fd3ed829b45324da8ac8af3840b35d2a4542
zhenbo.zheng <bleon1@126.com>
2014-01-07 22:55:03 +0800

    Create str.js

---
+++ src/str.js
@@ -0,0 +1,25 @@
+
+function startsWith (original, str) {
+    if (typeof String.prototype.startsWith != 'function') 
+        return original.slice(0, str.length) == str;
+    }
+    else {
+        return original.startsWith(original);
+    }
+}
+
+function endsWith (original, str) {
+    if (typeof String.prototype.endsWith != 'function') 
+        return original.slice(-str.length) == str;
+    }
+    else {
+        return original.endsWith(original);
+    }
+}
+
+methods = {
+    startsWith : startsWith,
+    endsWith : endsWith
+}
+
+module.exports = methods;
